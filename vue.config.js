4308f765b14f2e5fc9a6711078e6f83dc263a629
Joshua Ayeni <larryjmpk@gmail.com>
2022-03-31 01:27:42 +0100

    added new icon images

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    pwa:{
+        themeColor: '#17a2b8',
+        msTileColor: '#000000',
+        iconPaths:{
+            msTileImage: 'img/icons/mstile-150x150.png'
+        }
+    }
+}
